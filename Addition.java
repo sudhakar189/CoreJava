95cb57f3fab7f1cf01c6a876a7f276ef51a90ce8
Jhansi <jhansis2014@gmail.com>
2025-05-16 14:24:30 -0500

    variable add

--- Addition.java
+++ Addition.java
@@ -3,6 +3,7 @@ import java.sql.SQLOutput;
 public class Addition {
     public static void main(String[] args) {
         int x=2;
+        int y=4;
         System.out.println("addition");
     }
 }
